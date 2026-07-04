4220d665e522521577914f6a366434e71b6a121d
IIILLL3939 <cubing1224@gmail.com>
2026-07-04 11:15:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,32 @@ st.set_page_config(
     page_icon="🎉",
 )
 
-# 메인 화면
+# CSS
+st.markdown("""
+<style>
+/* 배경색 */
+.stApp {
+    background-color: #4da6ff;
+}
+
+/* 버튼 가운데 정렬 */
+div.stButton {
+    display: flex;
+    justify-content: center;
+}
+
+/* 버튼 크기(선택) */
+div.stButton > button {
+    font-size: 18px;
+    padding: 0.6em 2em;
+}
+</style>
+""", unsafe_allow_html=True)
+
+# 제목
 st.title("안녕하세요")
 
 # 버튼
 if st.button("나도 인사하기"):
     st.success("🎉 첫 웹앱 제작을 축하합니다!")
-    st.balloons()   # 풍선 효과
-    st.snow()       # 추가 효과 (선택 사항)
+    st.balloons()
